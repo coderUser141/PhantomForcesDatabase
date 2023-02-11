e7f56a46caecd2529b2123037c72355cc99ddc13
Peter Lisulov <peter.lisulov@outlook.com>
2023-02-11 00:31:45 -0500

    fleshed out classes and added more classes

--- NETCoreWPF/WeaponClasses.cs
+++ NETCoreWPF/WeaponClasses.cs
@@ -1,46 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 
 namespace NETCoreWPF
 {
-
+    
     class Weapon
     {
-        protected string name;
-        protected int rank;
+
+        private string name;
+        private int rank;
+        private bool hasRank;
+
+        public Weapon(string name, bool hasRank, int rank)
+        {
+            this.name = name;
+            if (hasRank)
+            {
+                this.hasRank = true;
+                this.rank = rank;
+            } else
+            {
+                this.hasRank = false;
+                this.rank = 0;
+            }
+        }
+
+        protected string Name { get; set; }
+        protected int Rank { get; set; }
+        protected bool HasRank { get; set; }
+
     }
 
     class Carried
     {
-        protected double limbMultipler,
+
+        private double limbMultipler,
                             torsoMultiplier,
                             headMultiplier,
                             walkspeed;
+
+        public Carried(double lM, double tM, double hM, double ws)
+        {
+            this.limbMultipler = lM;
+            this.torsoMultiplier = tM;
+            this.headMultiplier = hM;
+            this.walkspeed = ws;
+        }
+
+        protected double LimbMultiplier { get; set; }
+        protected double TorsoMultiplier { get; set; }
+        protected double HeadMultiplier { get; set; }
+        protected double WalkSpeed { get; set; }
     }
+
+    /*class CarriedAux
+    {
+        //Gun GunAux();
+    }*/
 
     class Ranged
     {
-        protected double range1,
+        private double range1,
                         range2,
                         damageRange1,
                         damageRange2;
     }
 
-    class Gun : Weapon, Carried, Ranged
-    {
-        public string caliber;
-        public double firerate;
-        public int ammoCapacity;
-        public int magazineCapacity;
-        public string[] firemodes = new string[5];
-        public double penetration;
-        public double muzzleVelocity;
-        public double aimingWalkspeed;
-        public double reloadSpeed;
-        public double emptyReloadSpeed;
+    
 
-        public class Conversion
+
+    public class FireModeList
+    {
+        protected List<string> modes = new List<string>();
+        
+
+        public FireModeList(string[] firemodes)
+        {
+            foreach (string str in firemodes)
+            {
+                if (str == "automatic" || str == "auto")
+                {
+                    //FireMode Automatic = new FireMode()
+                }
+            }
+        }
+    }
+
+    class FireMode{
+
+        private double firerate;
+        private string mode;
+        private bool burst;
+        private int burstMode;
+
+        public FireMode(double firerate, string mode, bool burst, int burstMode)
+        {
+            this.firerate = firerate;
+            this.mode = mode;
+            if (burst)
+            {
+                switch (burstMode)
+                {
+
+                }
+            }
+        }
+
+    }
+
+
+    class Gun : Weapon
+    {
+        
+
+        private FireModeList fireModes;
+        private string caliber;
+        private int ammoCapacity;
+        private int magazineCapacity;
+        private double penetration;
+        private double muzzleVelocity;
+        private double aimingWalkspeed;
+        private double reloadSpeed;
+        private double emptyReloadSpeed;
+
+        public Gun(string name, bool hasRank, int rank, string caliber, int ammoCapacity, int magazineCapacity, string[] firemodes, double penetration, double muzzleVelocity, double aimingWalkspeed, double reloadSpeed, double emptyReloadSpeed) : base(name, hasRank, rank)
+        {
+            this.caliber = caliber;
+            this.ammoCapacity = ammoCapacity;
+            this.emptyReloadSpeed = emptyReloadSpeed;
+            this.magazineCapacity = magazineCapacity;
+            this.aimingWalkspeed = aimingWalkspeed;
+            this.reloadSpeed = reloadSpeed;
+            this.muzzleVelocity = muzzleVelocity;
+            this.penetration = penetration;
+            this.fireModes = new FireModeList(firemodes);
+        }
+
+        public class Conversion//abstract class 
         {
             public string attachment;
             public string conversionName;
@@ -56,7 +155,7 @@ namespace NETCoreWPF
         public double backStabDamage;
         public double bladeLength;
 
-
+        
     }
 
     class Grenade : Weapon, Ranged
